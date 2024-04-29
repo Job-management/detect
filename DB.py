f086838f45e77058fac89b1492f0b3afe8b6d52d
vth20 <vanthanhhuynhtk@gmail.com>
2024-04-29 22:22:44 +0700

    [UPDATE] change process save data

---
+++ DB.py
@@ -0,0 +1,28 @@
+import mysql.connector
+from venv import logger
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def save_data_into_DB(data):
+    try:
+        connection = mysql.connector.connect(
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            database="job-management",
+        )
+        cursor = connection.cursor()
+
+        # query = "INSERT INTO `crawl_data` (`id`,`title`,`time`,`city`,`level`,`work_way`,`company`,`workWay`,`right`,`company`,`job`,`place`,`numberEmployee`,`experience`,`level`,`salary`,`education`,`description`,`requirement`,`deadline`,`images`,`link`,`type`,`contact`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        sql = "INSERT INTO crawl_data ({}) VALUES ({})".format(
+            ", ".join(data.keys()), ", ".join(["%s"] * len(data))
+        )
+        cursor.execute(sql, list(map(str, data.values())))
+        connection.commit()
+        connection.close()
+    except Exception as e:
+        logger.error(f"Error occurred while saving data to DB: {e}")
